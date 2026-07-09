3941d8e355c8b9cc65558996f4be47ec4697ebd4
Prashant <prshant.work@gmail.com>
2026-07-09 02:38:53 +0000

    fixed type errors

--- frontend/src/components/providers/theme-provider.tsx
+++ frontend/src/components/providers/theme-provider.tsx
@@ -3,10 +3,9 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  );
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
